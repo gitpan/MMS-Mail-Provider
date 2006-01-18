fe06fb621f1dba390986184fad85df5ba7f91a40
Rob Lee <robl@cpan.org>
2006-01-18 01:05:00 +0000

    Import of ROBL/MMS-Mail-Provider-0.06 from CPAN.
    gitpan-cpan-distribution: MMS-Mail-Provider
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         ROBL/MMS-Mail-Provider-0.06.tar.gz
    gitpan-cpan-author:       ROBL
    gitpan-cpan-maturity:     released

--- lib/MMS/Mail/Provider.pm
+++ lib/MMS/Mail/Provider.pm
@@ -11,11 +11,11 @@ MMS::Mail::Provider - This provides a base class for parsing an MMS::Mail::Messa
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 =head1 SYNOPSIS
 
